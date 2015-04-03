6e39638b802307786db43c06eab3d29fecd571a7
emilsalib <salibeh@jmu.edu>
2015-04-02 21:39:59 -0400

    Forth Commit

---
+++ 306_API_Current/app/mail.js
@@ -0,0 +1,59 @@
+var mysql = require('mysql');
+var new_results = ''; 	
+var connection =  mysql.createConnection({
+  	host : '134.126.67.231',
+  	user : 'root',
+  	password: 'isat306sec1',
+	database: 'mydb',
+	port: 3306,
+  }); 
+
+connection.query('SELECT * FROM `admin`', function (error, results, fields) {
+
+	if(error){
+ 		console.log(error);
+ 	};
+
+ 	for (var i = 0; i < results.length; i++){
+		
+		new_results = results[i].Email;
+
+ 	}
+
+  });
+
+//import nodemailer module
+var nodemailer = require('nodemailer');
+
+exports.mailScript = function (room_id, current_ppl, max_ppl){
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+    service: 'aol',
+    auth: {
+        user: 'ur3cadm1n@aol.com',
+        pass: 'isat306sec1'
+    }
+});
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: 'UREC Admin <ur3cadm1n@aol.com>', 
+    to: new_results, 
+    subject: 'UREC Alert: Maximum Room Capacity Reached',
+    html: '<b>Hello Admin,<br><br>'
+	+ 'The maximum capacity for the following room has been reached: </b><br>'
+	+ '<ul><li>Room ID: ' + room_id + '</li>' + '<ul><li>Current Occupancy: ' + current_ppl + '</li>' + '<li>Max Occupancy: ' + max_ppl + '</li></ul></ul>'
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    }
+});
+
+}
+
