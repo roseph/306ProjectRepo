4303ad5494908bd7745b72b70dfe088a23b38cf2
Paul <roseph148@gmail.com>
2015-04-02 22:46:16 -0400

    Cleaned Up Directories

--- 306_API_Old/config/dbModule.js
+++
@@ -1,8 +0,0 @@
-//var mongoose = require('mongoose');
-
-module.exports = {
-
-	'url': 'mongodb://api:api@ds041238.mongolab.com:41238/textbookexchangedb'
-	//'mongodb://localhost/TextbookExchangeDB'
-
-};
--- 306_Front_End/lineGraph.js
+++
@@ -1,6 +0,0 @@
-function drawChart() {
-
-      getHistoricalData();
-
-
-    }
